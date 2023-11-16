81a9aa551cc35b96163159b27ada6e90911ed118
YableZhao <yubol36400196@gmail.com>
2023-11-16 14:18:41 -0500

    modified basic functions, can only do 2 numbers calculations

--- Calculator.py
+++ Calculator.py
@@ -6,7 +6,18 @@ root.title("Simple calculator")
 entry = tk.Entry(root, width=35, borderwidth=5)
 entry.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
 
-# Operations
+# Global variables to store numbers and operations
+numbers = []
+operations = []
+
+# Operation functions
+def handle_operation(operation):
+    current_number = entry.get()
+    if current_number:
+        numbers.append(int(current_number))
+        operations.append(operation)
+        entry.delete(0, tk.END)
+
 def button_click(number):
     current = entry.get()
     entry.delete(0, tk.END)
@@ -16,68 +27,53 @@ def button_clear():
     entry.delete(0, tk.END),
 
 def button_add():
-    first_number = entry.get()
-    global f_num
-    global math
-    math = "addition"
-    f_num = int(first_number)
-    entry.delete(0, tk.END),
+    handle_operation("addition")
 
 def button_subtract():
-    first_number = entry.get()
-    global f_num
-    global math
-    math = "subtraction"
-    f_num = int(first_number)
-    entry.delete(0, tk.END),
+    handle_operation("subtraction")
 
 def button_multiply():
-    first_number = entry.get()
-    global f_num
-    global math
-    math = "multiplication"
-    f_num = int(first_number)
-    entry.delete(0, tk.END)
+    handle_operation("multiplication")
     
 def button_divide():
-    first_number = entry.get()
-    global f_num
-    global math
-    math = "division"
-    f_num = int(first_number)
-    entry.delete(0, tk.END)
+    handle_operation("division")
 
 def button_equal():
-    second_number = entry.get()
+    numbers.append(int(entry.get()))
     entry.delete(0, tk.END)
-    if math == "addition":
-        entry.insert(0, f_num + int(second_number))
-    if math == "subtraction":
-        entry.insert(0, f_num - int(second_number))
-    if math == "multiplication":
-        entry.insert(0, f_num * int(second_number))
-    if math == "division":
-        entry.insert(0, f_num / int(second_number))
+    result = numbers[0]
+    for i in range(len(operations)):
+        if operations[i] == "addition":
+            result += numbers[i + 1]
+        elif operations[i] == "subtraction":
+            result -= numbers[i + 1]
+        elif operations[i] == "multiplication":
+            result *= numbers[i + 1]
+        elif operations[i] == "division":
+            result /= numbers[i + 1]
+    entry.insert(0, result)
+    numbers.clear()
+    operations.clear()
 
 #buttons
+button_width = 3
+button_1 = tk.Button(root, text="1", padx=40, pady=20, width = button_width, command=lambda: button_click(1))
+button_2 = tk.Button(root, text="2", padx=40, pady=20, width = button_width, command=lambda: button_click(2))
+button_3 = tk.Button(root, text="3", padx=40, pady=20, width = button_width, command=lambda: button_click(3))
+button_4 = tk.Button(root, text="4", padx=40, pady=20, width = button_width, command=lambda: button_click(4))
+button_5 = tk.Button(root, text="5", padx=40, pady=20, width = button_width, command=lambda: button_click(5))
+button_6 = tk.Button(root, text="6", padx=40, pady=20, width = button_width, command=lambda: button_click(6))
+button_7 = tk.Button(root, text="7", padx=40, pady=20, width = button_width, command=lambda: button_click(7))
+button_8 = tk.Button(root, text="8", padx=40, pady=20, width = button_width, command=lambda: button_click(8))
+button_9 = tk.Button(root, text="9", padx=40, pady=20, width = button_width, command=lambda: button_click(9))
+button_0 = tk.Button(root, text="0", padx=40, pady=20, width = button_width, command=lambda: button_click(0))
 
-button_1 = tk.Button(root, text="1", padx=40, pady=20, command=lambda: button_click(1))
-button_2 = tk.Button(root, text="2", padx=40, pady=20, command=lambda: button_click(2))
-button_3 = tk.Button(root, text="3", padx=40, pady=20, command=lambda: button_click(3))
-button_4 = tk.Button(root, text="4", padx=40, pady=20, command=lambda: button_click(4))
-button_5 = tk.Button(root, text="5", padx=40, pady=20, command=lambda: button_click(5))
-button_6 = tk.Button(root, text="6", padx=40, pady=20, command=lambda: button_click(6))
-button_7 = tk.Button(root, text="7", padx=40, pady=20, command=lambda: button_click(7))
-button_8 = tk.Button(root, text="8", padx=40, pady=20, command=lambda: button_click(8))
-button_9 = tk.Button(root, text="9", padx=40, pady=20, command=lambda: button_click(9))
-button_0 = tk.Button(root, text="0", padx=40, pady=20, command=lambda: button_click(0))
-
-button_add = tk.Button(root, text="+", padx=100, pady=20, command=button_add)
-button_subtract = tk.Button(root, text="-",padx=100, pady=20, command=button_subtract)
-button_multiply = tk.Button(root, text="*", padx=40, pady=20, command=button_multiply)
-button_divide = tk.Button(root, text="/", padx=40, pady=20, command=button_divide)
-button_equal = tk.Button(root, text="=", padx=100, pady=20, command=button_equal)
-button_clear = tk.Button(root, text="AC", padx=100, pady=20, command=button_clear)
+button_add = tk.Button(root, text="+", padx=40, pady=20, width = button_width, command=button_add)
+button_subtract = tk.Button(root, text="-",padx=40, pady=20, width = button_width, command=button_subtract)
+button_multiply = tk.Button(root, text="*", padx=40, pady=20, width = button_width, command=button_multiply)
+button_divide = tk.Button(root, text="/", padx=40, pady=20, width = button_width, command=button_divide)
+button_equal = tk.Button(root, text="=", padx=40, pady=20, width = button_width, command=button_equal)
+button_clear = tk.Button(root, text="C", padx=40, pady=20, width = button_width, command=button_clear)
 
 # Display buttons
 button_1.grid(row=3, column=0)
@@ -93,10 +89,10 @@ button_0.grid(row=12, column=0)
 
 button_add.grid(row=3, column=3)
 button_subtract.grid(row=6, column = 3)
-button_multiply.grid(row=9, column = 4)
-button_divide.grid(row=12, column = 2)
-button_clear.grid(row=9, column=3)
-button_equal.grid(row=12, column=3)
+button_multiply.grid(row=9, column = 3)
+button_divide.grid(row=12, column = 3)
+button_clear.grid(row=12, column=1)
+button_equal.grid(row=12, column=2)
 
 
 root.mainloop()
